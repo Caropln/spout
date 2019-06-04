aac3e3e0a06fa696e84200fbefc8a44b53f4e8e4
MinosFenn <joachim.simon.45@gmail.com>
2019-06-04 14:58:37 +0200

    controller model  user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :reviews, foreign_key: 'review_id'
+  has_many :favorites_places, foreign_key: 'place_id'
+  validates :username, :firstname, :lastname, :address, :tel, :birth_date, presence: true
 end
