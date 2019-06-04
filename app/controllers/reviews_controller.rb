152070c5cc3d7f7bf306bd8632569249ad90b8ce
carolineplin <caroline.plin1@gmail.com>
2019-06-04 14:58:12 +0200

    controller routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,29 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to reviews_path(@place.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def find_place
+    @place = Place.find(params[:id])
+  end
 end
