bbe5e860677f4cbe2aebf9012af2b2658429bc3d
carolineplin <caroline.plin1@gmail.com>
2019-06-05 15:55:46 +0200

    homepage search bar autocomplete

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   has_many :reviews, foreign_key: 'review_id'
   has_many :favorites_places, foreign_key: 'place_id'
-  validates :username, :firstname, :lastname, :address, :tel, :birth_date, presence: true
+  # validates :firstname, :lastname, :address, :tel, :birth_date, presence: true
 end
