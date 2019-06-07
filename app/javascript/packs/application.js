aaed2f29c224f1325b182b98715d3d1d3053ee33
carolineplin <caroline.plin1@gmail.com>
2019-06-07 17:34:34 +0200

    removed useless css declaration

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,5 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
