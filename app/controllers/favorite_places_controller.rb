87364513ecca2a6f45b91229cb2ca74207c704a2
MinosFenn <joachim.simon.45@gmail.com>
2019-06-09 15:18:23 +0200

    favoritesplaces

--- app/controllers/favorite_places_controller.rb
+++
@@ -1,9 +0,0 @@
-class FavoritePlacesController < ApplicationController
-  def index
-    @favorites_places = FavoritePlaces.all
-  end
-
-  def create
-    @favorite_place = FavoritePlaces.new(user: current_user, places: params[:id])
-  end
-end
