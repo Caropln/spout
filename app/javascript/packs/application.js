87380a03ed2f9a7cff75e6872ffb5c04a9153dad
MinosFenn <joachim.simon.45@gmail.com>
2019-06-11 11:05:53 +0200

    text swaping +icon changed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
+import '../plugins/init_textswaping';
 
 
 initMapbox();
